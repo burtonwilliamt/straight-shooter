d50750708b8f1e13825875eaba6d9cec906cad10
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2025-11-17 21:30:37 -0500

    Multiplayer spawner custom func

--- multiplayer_test.gd
+++ multiplayer_test.gd
@@ -6,29 +6,27 @@ var peer = ENetMultiplayerPeer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	%PlayerSpawner.spawn_function = _player_spawn_func
 
 func _on_host_pressed():
 	peer.create_server(int(%Port.value))
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
-	_add_player(1)
+	%PlayerSpawner.spawn(1)
 	%Menu.visible = false
 
-func _add_player(id: int):
+# This gets called by the MultiplayerSpawner on every client to initialize
+# the player objects.
+# Do not call this function directly. Call the spawn() function instead.
+func _player_spawn_func(id: int) -> Node:
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	player.position = %PlayerSpawn.position
-	# TODO: Use a custom spawn function on the syncronizer with data for the spawn location
-	# OR: Just make an RPC that spawns the character
-	print(multiplayer.get_unique_id(), " Adding player at: ", player.position)
-	call_deferred("add_child", player)
+	return player
+
+func _add_player(id: int):
+	# TODO: Construct any additional spawn data here.
+	%PlayerSpawner.spawn(id)
 
 
 func _on_join_pressed():
